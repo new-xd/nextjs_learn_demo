51dcc3fb00ad9dde6324eff445f56349ca7c7f93
niuzonglin <niuzonglin@gmail.com>
2025-10-17 15:12:42 +0800

    feat: update to chapter 13

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -10,60 +10,83 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 import { z } from 'zod';
 
 const FormSchema = z.object({
-  id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
-  date: z.string(),
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
-    customerId: formData.get('customerId'),
-    amount: formData.get('amount'),
-    status: formData.get('status'),
-  });
-  const amountInCents = amount * 100;
-  const date = new Date().toISOString().split('T')[0];
-  console.log(`Creating invoice for customer ${customerId} with amount ${amount} and status ${status}`);
-  await sql`
+    const { customerId, amount, status } = CreateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+    console.log(`Creating invoice for customer ${customerId} with amount ${amount} and status ${status}`);
+    try {
+        await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
-  revalidatePath('/dashboard/invoices');
-  redirect('/dashboard/invoices');
+    } catch (error) {
+        console.error('Database Error:', error);
+        return {
+            message: 'Database Error: Failed to create invoice.',
+        };
+    }
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
 }
 
 
 // Use Zod to update the expected types
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
- 
+
 export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
-    customerId: formData.get('customerId'),
-    amount: formData.get('amount'),
-    status: formData.get('status'),
-  });
- 
-  const amountInCents = amount * 100;
- 
-  await sql`
+    const { customerId, amount, status } = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+
+    const amountInCents = amount * 100;
+
+    try {
+        await sql`
     UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
- 
-  revalidatePath('/dashboard/invoices');
-  redirect('/dashboard/invoices');
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+    } catch (error) {
+        console.error('Database Error:', error);
+        return {
+            message: 'Database Error: Failed to update invoice.',
+        };
+    }
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
 }
 
 
 export async function deleteInvoice(id: string) {
-  await sql`
+    throw new Error('Failed to Delete Invoice');
+    try {
+        await sql`
     DELETE FROM invoices WHERE id = ${id}
   `;
-  revalidatePath('/dashboard/invoices');
+    }
+    catch (error) {
+        console.error('Database Error:', error);
+        return {
+            message: 'Database Error: Failed to delete invoice.',
+        };
+    }
+    revalidatePath('/dashboard/invoices');
 }
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -159,11 +159,12 @@ export async function fetchInvoiceById(id: string) {
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
-
+    console.log('Invoice:', invoice);
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
-    throw new Error('Failed to fetch invoice.');
+    return null;
+    // throw new Error('Failed to fetch invoice.');
   }
 }
 
